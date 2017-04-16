662dc6f8e23ace866c4f565547a0c13b6ce262b5
Richard Greene <richard.a.greene@gmail.com>
2017-04-16 15:46:15 +0800

    ok

---
+++ MVXmap/MVXmap.DB/Model/Suburb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite.Net.Attributes;
+
+namespace MVXmap.DB
+{
+	public class Suburb
+	{
+		public Suburb()
+		{
+		}
+
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		[MaxLength(256)]
+		public string Name { get; set; }
+		public double Long { get; set; }
+		public double LAT { get; set; }
+	}
+}
