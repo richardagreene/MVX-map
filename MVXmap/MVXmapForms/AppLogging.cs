9ff5f238527b21d13c1a454d0a1102b73f599981
Richard Greene <richard.a.greene@gmail.com>
2017-04-21 11:14:49 +1000

    * MVXmap.Core.projitems: added missing files
    * AppLogging.cs:
    * MVXmap.Core.shproj:
    * MVXmap.UnitTests.csproj:
    * Contents.json:
    * Contents.json:

---
+++ MVXmap/MVXmapForms/AppLogging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MvvmCross.Platform;
+
+namespace MVXmapForms
+{
+	public static class AppLogging
+	{
+		static readonly string _tag = "MVXmap";
+
+		public static void Error(string message, params object[] args)
+		{
+			Mvx.TaggedError(_tag, message, args);
+		}
+
+		public static void Warning(string message, params object[] args)
+		{
+			Mvx.TaggedWarning(_tag, message, args);
+		}
+
+		public static void Debug(string message, params object[] args)
+		{
+			Mvx.TaggedTrace(_tag, message, args);
+		}
+	}
+}
